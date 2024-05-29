3f259f4f3f33d07d18301947447b6d0da21878ae
carlosgd17 <carlosgd_17@outlook.com>
2024-05-28 21:51:16 -0600

    db

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 	class Database {
 		private static $dbName = 'esp32_mc_db'; // Example: private static $dbName = 'myDB';
-		private static $dbHost = 'localhost'; // Example: private static $dbHost = 'localhost';
-		private static $dbUsername = 'root'; // Example: private static $dbUsername = 'myUserName';
+		private static $dbHost = 'restaurant-iot.mysql.database.azure.com'; // Example: private static $dbHost = 'localhost';
+		private static $dbUsername = 'restaurant'; // Example: private static $dbUsername = 'myUserName';
 		private static $dbUserPassword = '123456'; // // Example: private static $dbUserPassword = 'myPassword';
 		 
 		private static $cont  = null;
@@ -15,7 +15,11 @@
       // One connection through whole application
       if ( null == self::$cont ) {     
         try {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+          //self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+		  	self::$con = mysqli_init();
+			mysqli_ssl_set($con,NULL,NULL, "./DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+			mysqli_real_connect($conn, "restaurant-iot.mysql.database.azure.com", "restaurant", "N\$TG_UW6", "restaurant-iot", 3306, MYSQLI_CLIENT_SSL);
+			echo "<script>console.log('database' );</script>";
         } catch(PDOException $e) {
           die($e->getMessage()); 
         }
